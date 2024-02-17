9238c22eb5aa122f1452c63c0bede586f785658c
Sakshi Katore <katoresakshi@gmail.com>
2024-02-17 20:26:37 +0530

    veh

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -47,8 +47,8 @@ public class F1 extends Car {
         }
         //for all other cases, change the gear accordingly
 
-        if(newSpeed > 0) {
+        /*if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
-        }
+        }*/
     }
 }
